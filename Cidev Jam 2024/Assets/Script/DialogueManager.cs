6fca24d7545b5f4ca6d3b0b9af3e064f072d3b93
pokeizel <pokeizel@gmail.com>
2024-04-12 23:22:47 -0600

    commit

--- Cidev Jam 2024/Assets/Script/DialogueManager.cs
+++ Cidev Jam 2024/Assets/Script/DialogueManager.cs
@@ -18,6 +18,30 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("yay");
+        string s = dialogue.name + ": ";
+        for (int i = 0; i < dialogue.sentences.Length; i++) {
+            s += dialogue.sentences[i] + "\n";
+        }
+        //Debug.Log(s);
+
+        dialogue_lines.Clear();
+        foreach (string sentence in dialogue.sentences) {
+            dialogue_lines.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    void DisplayNextSentence() {
+        if (dialogue_lines.Count == 0) {
+            EndDialogue(); return;
+        }
+
+        string sentence = dialogue_lines.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue() {
+        Debug.Log("fin.");
     }
 }
